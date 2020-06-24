5fb351b55bdce54a6fdc8f8fa18aa759a1af5a6a
kiforks <ks.kifor@gmail.com>
2020-06-24 11:38:49 +0300

    Done

--- js/scripts/_jquery.magnific-popup.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('.projects__item').magnificPopup({
-    delegate: '.js-open-popup', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery: { enabled: true }
-  });
-});
